57bde589ba489b89e12dce53312c071f7177bd0f
artempapik <51436664+artempapik@users.noreply.github.com>
2021-11-07 21:25:56 +0200

    read description #2
    big refactoring; added some phrases; added weather responses

--- sofia_pleasant.rb
+++ sofia_pleasant.rb
@@ -1,24 +1,26 @@
 require 'crack'
 require 'net/http'
 require 'nokogiri'
+require 'openssl'
 require 'telegram/bot'
 require 'uri'
 
 TOKEN = '2003556781:AAGJRuQ7kEhcSlEYS5TSaZI6JwqDFtevVnI'
 POEMS_URL = 'https://www.culture.ru/literature/poems'
 HOROSCOPE_URL = 'https://ignio.com/r/export/utf/xml/daily/com.xml'
+WEATHER_URL = 'https://community-open-weather-map.p.rapidapi.com/weather?q=%s,ua&lat=0&lon=0&callback=test&id=2172797&lang=ru&units=imperial&mode=xml'
+WEATHER_TOKEN = '322872341amsh91f7c6576949217p10a086jsndcaed814811a'
 
-def get_html_document(url)
-  html = Net::HTTP.get(URI url)
-  Nokogiri::HTML html
-end
-
-def get_last_poem_page
-  document = get_html_document POEMS_URL
-  document.search('a.pagination_item.js-pagination-item')[-1].text.to_i
-end
-
-LAST_POEM_PAGE = get_last_poem_page
+@constants = {
+  :compliment               => 'комплимент',
+  :advice                   => 'совет',
+  :rude                     => 'быконуть',
+  :poem                     => 'стих',
+  :horoscope                => 'гораскоп',
+  :weather                  => 'погодо',
+  :tomorrow_horoscope       => 'хочу на завтра',
+  :after_tomorrow_horoscope => 'а можна на послезавтра?('
+}
 
 @greeting = IO.read 'greeting.txt'
 
@@ -62,6 +64,18 @@ def read_file_and_split(filename) = IO.readlines(filename).collect(&:strip)
 @argue_answers,
 @possible_argue_answers = file_paths.map(&method(:read_file_and_split))
 
+def get_html_document(url)
+  html = Net::HTTP.get(URI url)
+  Nokogiri::HTML html
+end
+
+def get_last_poem_page
+  document = get_html_document POEMS_URL
+  document.search('a.pagination_item.js-pagination-item')[-1].text.to_i
+end
+
+LAST_POEM_PAGE = get_last_poem_page
+
 def get_random_poem
   document = get_html_document("#{POEMS_URL}?page=#{rand(1..LAST_POEM_PAGE + 1)}")
 
@@ -76,18 +90,18 @@ def get_random_poem
 end
 
 @ru_to_en_horoscope_sign = {
-  'овен' => 'aries',
-  'телец' => 'taurus',
+  'овен'     => 'aries',
+  'телец'    => 'taurus',
   'близнецы' => 'gemini',
-  'рак' => 'cancer',
-  'лев' => 'leo',
-  'дева' => 'virgo',
-  'весы' => 'libra',
+  'рак'      => 'cancer',
+  'лев'      => 'leo',
+  'дева'     => 'virgo',
+  'весы'     => 'libra',
   'скорпион' => 'scorpio',
-  'стрелец' => 'sagittarius',
-  'козерог' => 'capricorn',
-  'водолей' => 'aquarius',
-  'рыбы' => 'pisces'
+  'стрелец'  => 'sagittarius',
+  'козерог'  => 'capricorn',
+  'водолей'  => 'aquarius',
+  'рыбы'     => 'pisces'
 }
 
 def get_horoscope(sign, day = 'today')
@@ -98,68 +112,112 @@ def get_horoscope(sign, day = 'today')
   "#{sign.upcase}\n#{horoscope[en_sign][day]}"
 end
 
-def tg_button(text) = Telegram::Bot::Types::KeyboardButton.new(text: text)
+@abbreviation_to_city = {
+  'каменск' => 'Kamianske',
+  'влн' => 'Vilniansk',
+  'зп' => 'Zaporijia',
+  'млт' => 'Melitopol'
+}
+
+def to_celsius(fahrenheit) = (fahrenheit - 32) / 1.8
+
+def get_temp(response, type) = to_celsius(response['main']["temp_#{type}"]).round(1)
+
+def get_start_end_day(response, type)
+  daytime = Time.at(response['sys'][type])
+  "#{daytime.hour}:#{daytime.min}"
+end
+
+def get_weather(abbreviation)
+  url = URI WEATHER_URL % @abbreviation_to_city[abbreviation]
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new url
+  request['x-rapidapi-key'] = WEATHER_TOKEN
+
+  response = http.request request
+  weather_response = JSON.parse(response.read_body[5..-2])
+
+  city_name = abbreviation == 'влн' ? 'Вольнянск' : weather_response['name']
+  sunrise = get_start_end_day(weather_response, 'sunrise')
+  sunset = get_start_end_day(weather_response, 'sunset')
+  description = weather_response['weather'].map { |weather| weather['description'] }.join(', ')
+  wind_speed = weather_response['wind']['speed']
+  temp_min = get_temp(weather_response, 'min')
+  temp_max = get_temp(weather_response, 'max')
+
+  city_name = "вы выбрали #{city_name})"
+  sunrise = "восход у нас в #{sunrise} утра"
+  sunset = "а закат в #{sunset} вечора"
+  description = "как оно: #{description}"
+  wind_speed = "скорость ветра при этом составляет #{wind_speed} м/с"
+  temp_min = "ну само прохладно будет при #{temp_min}°C"
+  temp_max = "а само тепло при #{temp_max}°C"
+  goodbye = "хорошего вам дня досвидания"
+
+  "#{city_name}\n\n#{sunrise}, #{sunset}\n#{description}\n\n#{wind_speed}\n\n#{temp_min}\n#{temp_max}\n\n\n#{goodbye}"
+end
 
 def inline_tg_button(text) = Telegram::Bot::Types::InlineKeyboardButton.new(text: text, callback_data: text)
 
-TEXT_TYPE = {
-  :none => 0,
-  :rude => 1,
-  :horoscope => 2
-}
-  
-def send_message(text, text_type, markup)
-  message_to_delete = @bot.api.send_message chat_id: @chat_id, text: text, reply_markup: markup
-  @message_id_to_delete = message_to_delete['result']['message_id'] if text_type == TEXT_TYPE[:horoscope]
-  @bot.api.send_sticker chat_id: @chat_id, sticker: @rude_stickers.sample if text_type == TEXT_TYPE[:rude]
-end
-
-def get_start_keyboard_markup
-  keyboard = [
-    [tg_button('комплимент'), tg_button('совет')],
-    [tg_button('быконуть'), tg_button('стих')],
-    [tg_button('гараскоп')]
-  ]
-  Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: keyboard)
-end
-
-def get_inline_keyboard_markup(text)
-  keyboard = inline_tg_button(text)
+def get_inline_keyboard_markup(*texts)
+  keyboard = texts.map(&method(:inline_tg_button))
   Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard)
 end
 
+def tg_button(text) = Telegram::Bot::Types::KeyboardButton.new(text: text)
+
 def get_text_with_type_and_reply_markup_from_message(message)
+  start_keyboard_markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [
+    [tg_button(@constants[:compliment]), tg_button(@constants[:advice])],
+    [tg_button(@constants[:rude]),       tg_button(@constants[:poem])],
+    [tg_button(@constants[:horoscope]),  tg_button(@constants[:weather])]
+  ])
+
+  horoscope_signs = @ru_to_en_horoscope_sign.keys
+  cities = @abbreviation_to_city.keys
+
   case message
 
   when '/start'
-    return @greeting, TEXT_TYPE[:none], get_start_keyboard_markup
+    return @greeting, nil, Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [
+      [tg_button(@constants[:compliment]), tg_button(@constants[:advice])],
+      [tg_button(@constants[:rude]),       tg_button(@constants[:poem])],
+      [tg_button(@constants[:horoscope]),  tg_button(@constants[:weather])]
+    ])
 
-  when 'комплимент'
+  when @constants[:compliment]
     "#{@compliments.sample} #{@pleasant_smiles.sample}"
 
-  when 'совет'
+  when @constants[:advice]
     @advices.sample
 
-  when 'быконуть'
-    return @rude_phrases.sample, TEXT_TYPE[:rude]
+  when @constants[:rude]
+    return @rude_phrases.sample, @constants[:rude]
 
-  when 'стих'
+  when @constants[:poem]
     get_random_poem
 
-  when 'гараскоп'
-    keyboard = @ru_to_en_horoscope_sign.keys.map(&method(:inline_tg_button))
-    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: keyboard)
-    return @horoscope_answers.sample, TEXT_TYPE[:horoscope], markup
+  when @constants[:horoscope]
+    return @horoscope_answers.sample, @constants[:horoscope], get_inline_keyboard_markup(*horoscope_signs)
 
-  when *@ru_to_en_horoscope_sign.keys
+  when *horoscope_signs
     @chosen_sign = message
-    return get_horoscope(message), TEXT_TYPE[:none], get_inline_keyboard_markup('хочу на завтра')
+    return get_horoscope(@chosen_sign), nil, get_inline_keyboard_markup(@constants[:tomorrow_horoscope])
 
-  when 'хочу на завтра'
-    return get_horoscope(@chosen_sign, 'tomorrow'), TEXT_TYPE[:none], get_inline_keyboard_markup('а можна на послезавтра?(')
+  when @constants[:tomorrow_horoscope]
+    return get_horoscope(@chosen_sign, 'tomorrow'), nil, get_inline_keyboard_markup(@constants[:after_tomorrow_horoscope])
 
-  when 'а можна на послезавтра?('
-    return get_horoscope(@chosen_sign, 'tomorrow02'), TEXT_TYPE[:none], get_start_keyboard_markup
+  when @constants[:after_tomorrow_horoscope]
+    return get_horoscope(@chosen_sign, 'tomorrow02')
+
+  when @constants[:weather]
+    return 'выбирай с хорошыми дорогами', @constants[:weather], get_inline_keyboard_markup(*cities)
+
+  when *cities
+    return get_weather(message)
 
   when *@possible_yes_no_answers
     @yes_no_answers.sample
@@ -173,15 +231,39 @@ def get_text_with_type_and_reply_markup_from_message(message)
   when *@possible_argue_answers
     @argue_answers.sample
 
+  when 'почему молчишь?'
+    'ты что меня не слышишь?'
+
+  when 'мне не говоришь'
+    'что без меня не дышыш..'
+
+  when 'кароче'
+    'понятна'
+
+  when 'тебе с ним приятно'
+    'ему с тобой тоже?'
+
+  when 'любовь'
+    'диалоги'
+
+  when 'повтор'
+    'и обратно'
+
   when '/goodbye'
     text = "#{@sad_phrases.sample} #{@sad_smiles.sample}"
-    markup = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-    return text, TEXT_TYPE[:none], markup
+    remove_keyboard_markup = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+    return text, nil, remove_keyboard_markup
 
   else
     @unable_responses.sample
 
   end
+end
+
+def send_message(text, text_type, markup)
+  message_to_delete = @bot.api.send_message chat_id: @chat_id, text: text, reply_markup: markup
+  @message_id_to_delete = message_to_delete['result']['message_id'] if text_type == @constants[:horoscope] or text_type == @constants[:weather]
+  @bot.api.send_sticker chat_id: @chat_id, sticker: @rude_stickers.sample if text_type == @constants[:rude]
 end
 
 def run_bot
@@ -193,7 +275,7 @@ def run_bot
       @chat_id = message[:chat][:id] if message[:chat]
   
       if @message_id_to_delete
-        @bot.api.edit_message_reply_markup(chat_id: @chat_id, message_id: @message_id_to_delete)
+        @bot.api.edit_message_reply_markup chat_id: @chat_id, message_id: @message_id_to_delete
         @message_id_to_delete = nil
       end
   
@@ -208,18 +290,3 @@ def run_bot
 end
 
 run_bot
-
-# require 'uri'
-# require 'net/http'
-# require 'openssl'
-
-# url = URI 'https://community-open-weather-map.p.rapidapi.com/weather?q=kamianske,ua&lat=0&lon=0&callback=test&id=2172797&lang=ru&units=imperial&mode=xml'
-
-# http = Net::HTTP.new(url.host, url.port)
-# http.use_ssl = true
-
-# request = Net::HTTP::Get.new url
-# request['x-rapidapi-key'] = '322872341amsh91f7c6576949217p10a086jsndcaed814811a'
-
-# response = http.request request
-# puts response.read_body
